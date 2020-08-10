6670e45d357da28293116e135e70d61a2e08a416
TobiasOnGitHub <tobias-schwarz@outlook.com>
2020-08-10 17:18:25 +0200

    JavaDoc generated.

--- doc/package-search-index.js
+++ doc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"se.gu.faxe"}]
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"se.gu.faxe"},{"l":"se.gu.faxe.commands"}]
