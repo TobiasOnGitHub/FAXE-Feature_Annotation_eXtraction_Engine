88a8ee0a2d2817cc14a505f53782306ab2f7d84f
TobiasOnGitHub <tobias-schwarz@outlook.com>
2020-08-10 17:24:00 +0200

    Regenerate grammar to correct location

--- src/main/java/se/gu/faxe/grammar/featureHierarchyLexer.java
+++ src/main/java/se/gu/faxe/grammar/featureHierarchyLexer.java
@@ -1,10 +1,13 @@
-// Generated from C:/Users/Tobias/IdeaProjects/FAXE-Feature_Annotation_eXtraction_Engine\featureHierarchy.g4 by ANTLR 4.8
+// Generated from C:/Users/Tobias/IdeaProjects/FAXE-Feature_Annotation_eXtraction_Engine/src/main/antlr4/se/gu/faxe\featureHierarchy.g4 by ANTLR 4.8
 package se.gu.faxe.grammar;
 import org.antlr.v4.runtime.Lexer;
 import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.Token;
+import org.antlr.v4.runtime.TokenStream;
 import org.antlr.v4.runtime.*;
 import org.antlr.v4.runtime.atn.*;
 import org.antlr.v4.runtime.dfa.DFA;
+import org.antlr.v4.runtime.misc.*;
 
 @SuppressWarnings({"all", "warnings", "unchecked", "unused", "cast"})
 public class featureHierarchyLexer extends Lexer {
--- src/main/java/se/gu/faxe/grammar/featureHierarchyParser.java
+++ src/main/java/se/gu/faxe/grammar/featureHierarchyParser.java
@@ -1,10 +1,13 @@
-// Generated from C:/Users/Tobias/IdeaProjects/FAXE-Feature_Annotation_eXtraction_Engine\featureHierarchy.g4 by ANTLR 4.8
+// Generated from C:/Users/Tobias/IdeaProjects/FAXE-Feature_Annotation_eXtraction_Engine/src/main/antlr4/se/gu/faxe\featureHierarchy.g4 by ANTLR 4.8
 package se.gu.faxe.grammar;
 import org.antlr.v4.runtime.atn.*;
 import org.antlr.v4.runtime.dfa.DFA;
 import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.misc.*;
 import org.antlr.v4.runtime.tree.*;
 import java.util.List;
+import java.util.Iterator;
+import java.util.ArrayList;
 
 @SuppressWarnings({"all", "warnings", "unchecked", "unused", "cast"})
 public class featureHierarchyParser extends Parser {
