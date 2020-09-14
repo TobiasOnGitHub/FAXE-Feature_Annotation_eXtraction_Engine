cfdbffe0adf6f61671c59b7327334fb83865f610
TobiasOnGitHub <tobias-schwarz@outlook.com>
2020-09-14 15:16:26 +0200

    Disable Feature class parent and subFeature handling as this functionality is now provided by TreeNode library.

--- src/test/java/se/gu/faxe/FeatureTest.java
+++ src/test/java/se/gu/faxe/FeatureTest.java
@@ -21,9 +21,6 @@ package se.gu.faxe;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import java.util.Arrays;
-import java.util.List;
-
 public class FeatureTest {
 
     @Test
@@ -40,168 +37,168 @@ public class FeatureTest {
         new Feature(null);
     }
 
-    @Test
-    public void testFeature_StringFeature_ValidContent(){
-        String name = "root";
-        Feature parent = new Feature("parent");
+//    @Test
+//    public void testFeature_StringFeature_ValidContent(){
+//        String name = "root";
+//        Feature parent = new Feature("parent");
+//
+//        Feature root = new Feature(name, parent);
+//
+//        Assert.assertNotNull(root);
+//    }
 
-        Feature root = new Feature(name, parent);
+//    @Test(expectedExceptions = NullPointerException.class, expectedExceptionsMessageRegExp = "Feature::Feature name is null!")
+//    public void testFeature_StringFeature_NullContent_name(){
+//        Feature parent = new Feature("parent");
+//
+//        new Feature(null, parent);
+//    }
 
-        Assert.assertNotNull(root);
-    }
+//    @Test(expectedExceptions = NullPointerException.class, expectedExceptionsMessageRegExp = "Feature::Feature parent is null!")
+//    public void testFeature_StringFeature_NullContent_parent(){
+//        String name = "root";
+//
+//        new Feature(name, null);
+//    }
 
-    @Test(expectedExceptions = NullPointerException.class, expectedExceptionsMessageRegExp = "Feature::Feature name is null!")
-    public void testFeature_StringFeature_NullContent_name(){
-        Feature parent = new Feature("parent");
+//    @Test
+//    public void testGetSubFeatures_ValidContent_emptySubFeature() {
+//        Feature root = new Feature("root");
+//
+//        List<Feature> ret = root.getSubFeatures();
+//
+//        Assert.assertEquals(ret.size(), 0);
+//    }
 
-        new Feature(null, parent);
-    }
+//    @Test
+//    public void testAddSubFeature_ValidContent_AddValue() {
+//        Feature root = new Feature("root");
+//        Feature sub1 = new Feature("sub1");
+//
+//        Boolean ret = root.addSubFeature(sub1);
+//
+//        Assert.assertEquals(ret, Boolean.TRUE);
+//    }
 
-    @Test(expectedExceptions = NullPointerException.class, expectedExceptionsMessageRegExp = "Feature::Feature parent is null!")
-    public void testFeature_StringFeature_NullContent_parent(){
-        String name = "root";
+//    @Test
+//    public void testAddSubFeature_ValidContent_twoValuesIndividual_size() {
+//        Feature root = new Feature("root");
+//        Feature sub1 = new Feature("sub1");
+//        Feature sub2 = new Feature("sub2");
+//
+//        root.addSubFeature(sub1);
+//        root.addSubFeature(sub2);
+//        List<Feature> ret = root.getSubFeatures();
+//
+//        Assert.assertEquals(ret.size(), 2);
+//    }
 
-        new Feature(name, null);
-    }
+//    @Test
+//    public void testAddSubFeature_ValidContent_twoValuesIndividual_content() {
+//        Feature root = new Feature("root");
+//        Feature sub1 = new Feature("sub1");
+//        Feature sub2 = new Feature("sub2");
+//
+//        root.addSubFeature(sub1);
+//        root.addSubFeature(sub2);
+//        List<Feature> ret = root.getSubFeatures();
+//
+//        Assert.assertTrue(ret.contains(sub1));
+//        Assert.assertTrue(ret.contains(sub2));
+//    }
 
-    @Test
-    public void testGetSubFeatures_ValidContent_emptySubFeature() {
-        Feature root = new Feature("root");
-
-        List<Feature> ret = root.getSubFeatures();
-
-        Assert.assertEquals(ret.size(), 0);
-    }
-
-    @Test
-    public void testAddSubFeature_ValidContent_AddValue() {
-        Feature root = new Feature("root");
-        Feature sub1 = new Feature("sub1");
-
-        Boolean ret = root.addSubFeature(sub1);
-
-        Assert.assertEquals(ret, Boolean.TRUE);
-    }
-
-    @Test
-    public void testAddSubFeature_ValidContent_twoValuesIndividual_size() {
-        Feature root = new Feature("root");
-        Feature sub1 = new Feature("sub1");
-        Feature sub2 = new Feature("sub2");
-
-        root.addSubFeature(sub1);
-        root.addSubFeature(sub2);
-        List<Feature> ret = root.getSubFeatures();
-
-        Assert.assertEquals(ret.size(), 2);
-    }
-
-    @Test
-    public void testAddSubFeature_ValidContent_twoValuesIndividual_content() {
-        Feature root = new Feature("root");
-        Feature sub1 = new Feature("sub1");
-        Feature sub2 = new Feature("sub2");
-
-        root.addSubFeature(sub1);
-        root.addSubFeature(sub2);
-        List<Feature> ret = root.getSubFeatures();
-
-        Assert.assertTrue(ret.contains(sub1));
-        Assert.assertTrue(ret.contains(sub2));
-    }
-
-    @Test
-    public void testAddSubFeature_ValidContent_addValue() {
-        Feature root = new Feature("root");
-        List<Feature> list = Arrays.asList(
-                new Feature("sub1"),
-                new Feature("sub2")
-        );
-        Boolean ret = root.addSubFeature(list);
-
-        Assert.assertEquals(ret, Boolean.TRUE);
-    }
+//    @Test
+//    public void testAddSubFeature_ValidContent_addValue() {
+//        Feature root = new Feature("root");
+//        List<Feature> list = Arrays.asList(
+//                new Feature("sub1"),
+//                new Feature("sub2")
+//        );
+//        Boolean ret = root.addSubFeature(list);
+//
+//        Assert.assertEquals(ret, Boolean.TRUE);
+//    }
 
 
-    @Test
-    public void testAddSubFeature_ValidContent_twoValuesList_size() {
-        Feature root = new Feature("root");
-        List<Feature> list = Arrays.asList(
-                new Feature("sub1"),
-                new Feature("sub2")
-        );
-        root.addSubFeature(list);
+//    @Test
+//    public void testAddSubFeature_ValidContent_twoValuesList_size() {
+//        Feature root = new Feature("root");
+//        List<Feature> list = Arrays.asList(
+//                new Feature("sub1"),
+//                new Feature("sub2")
+//        );
+//        root.addSubFeature(list);
+//
+//        List<Feature> ret = root.getSubFeatures();
+//
+//        Assert.assertEquals(ret.size(), 2);
+//    }
 
-        List<Feature> ret = root.getSubFeatures();
+//    @Test(expectedExceptions = NullPointerException.class, expectedExceptionsMessageRegExp = "Feature::addSubFeature feature is null!")
+//    public void testAddSubFeature_InvalidContent_null_feature() {
+//        Feature root = new Feature("root");
+//
+//        root.addSubFeature((Feature)null);
+//    }
 
-        Assert.assertEquals(ret.size(), 2);
-    }
+//    @Test(expectedExceptions = NullPointerException.class, expectedExceptionsMessageRegExp = "Feature::addSubFeature feature list is null!")
+//    public void testAddSubFeature_InvalidContent_null_featureList() {
+//        Feature root = new Feature("root");
+//
+//        root.addSubFeature((List<Feature>)null);
+//    }
 
-    @Test(expectedExceptions = NullPointerException.class, expectedExceptionsMessageRegExp = "Feature::addSubFeature feature is null!")
-    public void testAddSubFeature_InvalidContent_null_feature() {
-        Feature root = new Feature("root");
+//    @Test
+//    public void testRemoveSubFeature_ValidContent_twoValuesList_content_addThreeValues_removeLast() {
+//        Feature root = new Feature("root");
+//        List<Feature> list = Arrays.asList(
+//                new Feature("sub1"),
+//                new Feature("sub2"),
+//                new Feature("sub3")
+//        );
+//
+//        root.addSubFeature(list);
+//        root.removeSubFeature(list.get(2));
+//
+//        List<Feature> ret = root.getSubFeatures();
+//
+//        Assert.assertTrue(ret.contains(list.get(0)));
+//        Assert.assertTrue(ret.contains(list.get(1)));
+//    }
 
-        root.addSubFeature((Feature)null);
-    }
+//    @Test
+//    public void testRemoveSubFeature_ValidContent_twoValuesList_content_addThreeValues_removeFirst() {
+//        Feature root = new Feature("root");
+//        List<Feature> list = Arrays.asList(
+//                new Feature("sub1"),
+//                new Feature("sub2"),
+//                new Feature("sub3")
+//        );
+//
+//        root.addSubFeature(list);
+//        root.removeSubFeature(list.get(0));
+//
+//        List<Feature> ret = root.getSubFeatures();
+//
+//        Assert.assertTrue(ret.contains(list.get(1)));
+//        Assert.assertTrue(ret.contains(list.get(2)));
+//    }
 
-    @Test(expectedExceptions = NullPointerException.class, expectedExceptionsMessageRegExp = "Feature::addSubFeature feature list is null!")
-    public void testAddSubFeature_InvalidContent_null_featureList() {
-        Feature root = new Feature("root");
-
-        root.addSubFeature((List<Feature>)null);
-    }
-
-    @Test
-    public void testRemoveSubFeature_ValidContent_twoValuesList_content_addThreeValues_removeLast() {
-        Feature root = new Feature("root");
-        List<Feature> list = Arrays.asList(
-                new Feature("sub1"),
-                new Feature("sub2"),
-                new Feature("sub3")
-        );
-
-        root.addSubFeature(list);
-        root.removeSubFeature(list.get(2));
-
-        List<Feature> ret = root.getSubFeatures();
-
-        Assert.assertTrue(ret.contains(list.get(0)));
-        Assert.assertTrue(ret.contains(list.get(1)));
-    }
-
-    @Test
-    public void testRemoveSubFeature_ValidContent_twoValuesList_content_addThreeValues_removeFirst() {
-        Feature root = new Feature("root");
-        List<Feature> list = Arrays.asList(
-                new Feature("sub1"),
-                new Feature("sub2"),
-                new Feature("sub3")
-        );
-
-        root.addSubFeature(list);
-        root.removeSubFeature(list.get(0));
-
-        List<Feature> ret = root.getSubFeatures();
-
-        Assert.assertTrue(ret.contains(list.get(1)));
-        Assert.assertTrue(ret.contains(list.get(2)));
-    }
-
-    @Test
-    public void testRemoveSubFeature_InvalidContent_RemoveNonSubFeature() {
-        Feature root = new Feature("root");
-        List<Feature> list = Arrays.asList(
-                new Feature("sub1"),
-                new Feature("sub2"),
-                new Feature("sub3")
-        );
-        Feature nonAddedFeature = new Feature("nonAddedFeature");
-
-        root.addSubFeature(list);
-        Boolean ret = root.removeSubFeature(nonAddedFeature);
-
-        Assert.assertEquals(ret, Boolean.FALSE);
-    }
+//    @Test
+//    public void testRemoveSubFeature_InvalidContent_RemoveNonSubFeature() {
+//        Feature root = new Feature("root");
+//        List<Feature> list = Arrays.asList(
+//                new Feature("sub1"),
+//                new Feature("sub2"),
+//                new Feature("sub3")
+//        );
+//        Feature nonAddedFeature = new Feature("nonAddedFeature");
+//
+//        root.addSubFeature(list);
+//        Boolean ret = root.removeSubFeature(nonAddedFeature);
+//
+//        Assert.assertEquals(ret, Boolean.FALSE);
+//    }
 
     @Test
     public void testRenameFeature_ValidContent() {
@@ -230,31 +227,31 @@ public class FeatureTest {
         Assert.assertEquals(root.getName(), name);
     }
 
-    @Test
-    public void testGetParent() {
-        Feature root = new Feature("root");
+//    @Test
+//    public void testGetParent() {
+//        Feature root = new Feature("root");
+//
+//        Feature parent = root.getParent();
+//
+//        Assert.assertNull(parent);
+//    }
 
-        Feature parent = root.getParent();
+//    @Test
+//    public void testSetParent_ValidContent() {
+//        Feature root = new Feature("root");
+//        Feature parent = new Feature("parent");
+//
+//        Boolean ret = root.setParent(parent);
+//
+//        Assert.assertEquals(ret, Boolean.TRUE);
+//    }
 
-        Assert.assertNull(parent);
-    }
-
-    @Test
-    public void testSetParent_ValidContent() {
-        Feature root = new Feature("root");
-        Feature parent = new Feature("parent");
-
-        Boolean ret = root.setParent(parent);
-
-        Assert.assertEquals(ret, Boolean.TRUE);
-    }
-
-    @Test(expectedExceptions = NullPointerException.class, expectedExceptionsMessageRegExp = "Feature::setParent parent is null!")
-    public void testSetParent_InvalidContent_Null() {
-        Feature root = new Feature("root");
-
-        root.setParent(null);
-    }
+//    @Test(expectedExceptions = NullPointerException.class, expectedExceptionsMessageRegExp = "Feature::setParent parent is null!")
+//    public void testSetParent_InvalidContent_Null() {
+//        Feature root = new Feature("root");
+//
+//        root.setParent(null);
+//    }
 
     @Test
     public void testGetFeature() {
