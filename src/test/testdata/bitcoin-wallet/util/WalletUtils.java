0ede8b5c8e6b7f9657cf2480ac5ed994e6d0b903
TobiasOnGitHub <tobias-schwarz@outlook.com>
2020-09-18 15:13:16 +0200

    Add bitcoin-wallet project for test purposes.

--- src/test/testdata/bitcoin-wallet/util/WalletUtils.java
+++ src/test/testdata/bitcoin-wallet/util/WalletUtils.java
@@ -34,7 +34,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.annotation.Nullable;
-import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.text.DateFormat;
 import java.text.ParseException;
