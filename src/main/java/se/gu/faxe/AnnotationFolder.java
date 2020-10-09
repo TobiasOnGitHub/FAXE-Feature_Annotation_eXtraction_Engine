fd1474b3e26d4b32638f999d4481d8ff6b064dfc
TobiasOnGitHub <tobias-schwarz@outlook.com>
2020-10-09 22:48:32 +0200

    Take care of own linkedFeatures list.

--- src/main/java/se/gu/faxe/AnnotationFolder.java
+++ src/main/java/se/gu/faxe/AnnotationFolder.java
@@ -5,7 +5,7 @@ import java.util.List;
 public class AnnotationFolder extends Annotation {
 
     AnnotationFolder(List<Feature> listFeature){
-        super();
+        linkedFeatures.addAll(listFeature);
     }
 
 }
