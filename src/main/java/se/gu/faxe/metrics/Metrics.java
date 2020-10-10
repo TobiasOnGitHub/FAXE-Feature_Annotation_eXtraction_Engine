a347cf6bf327f009d1cdd4c6f417608bf78a42eb
TobiasOnGitHub <tobias-schwarz@outlook.com>
2020-10-10 17:27:49 +0200

    Update metrics list which ones are possible considering only features.

--- src/main/java/se/gu/faxe/metrics/Metrics.java
+++ src/main/java/se/gu/faxe/metrics/Metrics.java
@@ -19,29 +19,29 @@
 package se.gu.faxe.metrics;
 
 public enum Metrics {
-    ALL     ("ALL"),
+    ALL      ("ALL"),
     /* FEATURE METRICS */
-    SD      ("SD"),     // Scattering Degree: total number of all annotations directly referencing the feature (i.e., in-file, folder, and file annotations referencing it)
-    NoFiA   ("NoFiA"),  // Number of File Annotations: total number of file annotations directly referencing the feature
-    NoFoA   ("NoFoA"),  // Number of Folder Annotations: total number of folder annotations directly referencing the feature.
-    TD      ("TD"),     // Tangling Degree: number of other features that share the same artifacts (or parts of such) with the feature. Two features share (parts of) artifacts when the latter is annotated with both features.
-    LoFC    ("LoFC"),   // Lines of Feature Code: lines of code belonging to artifacts, either directly annotated, or indirectly (when a folder is annotated, all descendants are taken into account)
-    AvgND   ("AvgND"),  /* Nesting depths of annotations: Maximum (MaxND), Minimum (MinND), and Average (AvgND) nesting depth the annotations directly referencing the feature. The project’s root folder has depth 0 (and so has any file contained in it). */
-    MaxND   ("MaxND"),  /* Each sub-folder increases the depth by one, a file inherits the depth of its containing folder. The depth of a (top-level, i.e., non-nested) in-file annotation is the depth of the file increased by one. Since in-file annotations */
-    MinND   ("MinND"),  /* can be nested, each nesting increases the depth by one. All nesting-depth metrics are calculated relative to the project root folder. */
-    NoAu    ("NoAu"),   // Number of Authors who contributed to a feature’s artifact. Author information is automatically extracted from author tags (format: “Author: firstname lastname”) in comments wrapped by “/**” and “*/” in the source code if they exist.
-    NoAF    ("NoAF"),   // Number of annotated files: counts the individual feature to file mappings in the mapping files feature-to-file.
+    SD       ("SD"),      // Scattering Degree: total number of all annotations directly referencing the feature (i.e., in-file, folder, and file annotations referencing it)
+    NoFiA    ("NoFiA"),   // Number of File Annotations: total number of file annotations directly referencing the feature
+    NoFoA    ("NoFoA"),   // Number of Folder Annotations: total number of folder annotations directly referencing the feature.
+    TD       ("TD"),      // Tangling Degree: number of other features that share the same artifacts (or parts of such) with the feature. Two features share (parts of) artifacts when the latter is annotated with both features.
+    LoFC     ("LoFC"),    // Lines of Feature Code: lines of code belonging to artifacts, either directly annotated, or indirectly (when a folder is annotated, all descendants are taken into account)
+    AvgND    ("AvgND"),   /* Nesting depths of annotations: Maximum (MaxND), Minimum (MinND), and Average (AvgND) nesting depth the annotations directly referencing the feature. The project’s root folder has depth 0 (and so has any file contained in it). */
+    MaxND    ("MaxND"),   /* Each sub-folder increases the depth by one, a file inherits the depth of its containing folder. The depth of a (top-level, i.e., non-nested) in-file annotation is the depth of the file increased by one. Since in-file annotations */
+    MinND    ("MinND"),   /* can be nested, each nesting increases the depth by one. All nesting-depth metrics are calculated relative to the project root folder. */
+    NoAu     ("NoAu"),    // Number of Authors who contributed to a feature’s artifact. Author information is automatically extracted from author tags (format: “Author: firstname lastname”) in comments wrapped by “/**” and “*/” in the source code if they exist.
+    NoAF     ("NoAF"),    // Number of Annotated Files: counts the individual feature to file mappings in the mapping files feature-to-file.
     /* FOLDER METRICS */
-    NoF     ("NoF"),    // Number of Features: total number of features directly referenced in annotations (folder, file, in-file) of the folder and any of its descendants
-    LoFoC   ("LoFoC"),  // Lines of Folder Code: total lines in any descendant file of the folder
-    NoFi    ("NoFi"),   //Number of Files: number of all descendant files of the folder
+    NoF      ("NoF"),     // Number of Features: total number of features directly referenced in annotations (folder, file, in-file) of the folder and any of its descendants
+    // LoFoC ("LoFoC"),      // Lines of Folder Code: total lines in any descendant file of the folder --> FAXE: NOT SUPPORTED AS NOT LINKED TO FEATURE CODE
+    // NoFi  ("NoFi"),       // Number of Files: number of all descendant files of the folder --> FAXE: NOT SUPPORTED AS NOT LINKED TO FEATURE CODE
     /* PROJECT METRICS */
-    // NoF  ("NoF")     // Number of features in project => With FAXE implicit given with path to root folder
-    // Total LoFC ("Total LoFC")   // Total Lines of Feature Code: sum of LoFC (all features) => With FAXE implicit given with path to root folder
-    AvgLoFC ("AvgLoFC"),// Average Feature Lines of Code: sum of LoFC (all features) / NoF
-    //AvgND ("AvgND"),     // Average Feature Nesting Depth: sum of ND (all features) / NoF
-    AvgSD   ("AvgSD"),  // Average Feature Scattering Degree: sum of SD (all features) / NoF
-    COUNT   ("COUNT");  // Internal enum for going through all of them.
+    // NoF   ("NoF"),        // Number of features in project --> FAXE: COVERED WITH FIRST NOF AS PATH IS CONFIGURABLE TO FILE/FOLDER
+    pTotLoFC ("pTotLoFC"),// Total Lines of Feature Code: sum of LoFC (all features)
+    pAvgLoFC ("pAvgLoFC"),// Average Feature Lines of Code: sum of LoFC (all features) / NoF
+    pAvgND   ("pAvgND"),  // Average Feature Nesting Depth: sum of ND (all features) / NoF
+    pAvgSD   ("pAvgSD"),  // Average Feature Scattering Degree: sum of SD (all features) / NoF
+    COUNT    ("COUNT");   // Internal enum for going through all of them.
 
     private final String s;
     Metrics(String s) {
